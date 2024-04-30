644050cba491da034cc63cb902e69b7f35ed007f
Roger Vargas <rogerfsvargas@hotmail.com>
2024-04-30 14:42:30 -0300

    fix: fix header links

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  constructor(private userService: UserService, private router: Router) {}
 
+  user$ = this.userService.getUser();
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['/login']);
+  }
 }
